28e97d49bc55d6f3a33b743ac68fa34c2b957035
Dustin <djourdan555@gmail.com>
2022-06-12 16:17:55 +0700

    define collector table for wallet

---
+++ collector/collector.go
@@ -0,0 +1,42 @@
+package collector
+
+import (
+	"context"
+
+	"github.com/dinorain/depositaja"
+	"github.com/dinorain/depositaja/pb"
+	"github.com/lovoo/goka"
+)
+
+var (
+	group goka.Group = "balance"
+	Table goka.Table = goka.GroupTable(group)
+)
+
+func collect(ctx goka.Context, msg interface{}) {
+	ml := &pb.DepositHistory{}
+	if v := ctx.Value(); v != nil {
+		ml = v.(*pb.DepositHistory)
+	}
+
+	m := msg.(*pb.Deposit)
+
+	ml.WalletId = m.WalletId
+	ml.Deposits = append(ml.Deposits, m)
+
+	ctx.SetValue(ml)
+}
+
+func Run(ctx context.Context, brokers []string) func() error {
+	return func() error {
+		g := goka.DefineGroup(group,
+			goka.Input(depositaja.DepositStream, new(depositaja.DepositCodec), collect),
+			goka.Persist(new(depositaja.DepositListCodec)),
+		)
+		p, err := goka.NewProcessor(brokers, g)
+		if err != nil {
+			return err
+		}
+		return p.Run(ctx)
+	}
+}
